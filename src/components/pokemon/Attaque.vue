f61fad68c4e10a26f80156d67c756d514cfbaa39
Salim YDJEDD <sydjedd@gmail.com>
2021-06-14 11:42:21 +0200

    Ajout panel Attaque

---
+++ src/components/pokemon/Attaque.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card min-width="260">
+    <v-card-subtitle class="text-uppercase text-center d-block">
+      Attaques
+    </v-card-subtitle>
+
+    <v-card-text>
+      <v-row>
+        <v-col
+          v-for="(element, index) in moves"
+          :key="index"
+          cols="4"
+          md="1"
+          sm="2"
+          xs="3"
+        >
+          {{ element.move.name }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Attaque",
+
+  // TODO Affichage en tableau plus couleur
+  props: {
+    moves: Array,
+  },
+};
+</script>
